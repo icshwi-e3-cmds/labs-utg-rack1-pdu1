3329b6bb5c2515999f26b6acd74ef8d84bf38fc4
Wayne Lewis <wayne.lewis@esss.se>
2018-11-26 23:35:02 +0100

    Update startup scripts for new path location

--- labs-utgard-rack1-pdu1.cmd
+++ labs-utgard-rack1-pdu1.cmd
@@ -1,10 +1,11 @@
 # Required modules
 ## module_name,version
 
-require snmp,1.0.0.1
-require iocStats,1856ef5
+require snmp,1.0.0.2
+require iocStats,ae5d083
 require autosave,5.9.0
 require recsync,1.3.0
+require caPutLog,3.6.0
 
 # Environment variables
 epicsEnvSet("ENGINEER", "Wayne Lewis")
@@ -14,12 +15,20 @@ epicsEnvSet("EPICS_IOC_LOG_INET", "localhost")
 epicsEnvSet("EPICS_IOC_LOG_PORT", "7004")
 iocLogInit()
 
+# Start caput logging
+epicsEnvSet("EPICS_AS_PUT_LOG_PV", "$(IOC):caPutLatest")
+epicsEnvSet("LOG_INET", "localhost")
+epicsEnvSet("LOG_INET_PORT", "7011")
+epicsEnvSet("ACCESS_SECURITY_PATH_IOC", "$(caPutLog_DB)")
+epicsEnvSet("ACCESS_SECURITY_FILE_IOC", "default.asg")
+
 ## Paths
 epicsEnvSet(TOP, "$(E3_CMD_TOP)")
 epicsEnvSet("AUTOSAVE_DIR", "/opt/nonvolatile_local")
 epicsEnvSet("IOCSTATS_CMD_TOP", "$(TOP)/../../e3-iocStats/cmds")
 epicsEnvSet("AUTOSAVE_CMD_TOP", "$(TOP)/../../e3-autosave/cmds")
 epicsEnvSet("SNMP_CMD_TOP", "$(TOP)/../../e3-snmp/cmds")
+epicsEnvSet("RECSYNC_CMD_TOP", "$(TOP)/../../e3-recsync/cmds")
 epicsEnvSet("RECSYNC_CMD_TOP", "$(TOP)/../../e3-recsync/cmds")
 
 ## Database macros
@@ -32,6 +41,8 @@ epicsEnvSet("IOC",  "$(P)-$(R)")
 epicsEnvSet("PDU_IP", "10.4.0.121")
 epicsEnvSet("RACK", "VIP Chopper")
 epicsEnvSet("PDU", "PDU1")
+
+iocshLoad "$(caPutLog_DIR)/caPutLog.iocsh" ")
 
 iocshLoad "$(IOCSTATS_CMD_TOP)/iocStats.cmd" "IOC=$(IOC):IocStats")
 
@@ -46,10 +57,6 @@ iocshLoad "$(RECSYNC_CMD_TOP)/recsync.cmd"
 iocInit()
 
 iocshLoad "$(AUTOSAVE_CMD_TOP)/save_restore_after_init.cmd" "P=$(P),R=$(R), IOC=$(IOC), AS_TOP=/opt/nonvolatile_local/$(IOC)"
-
-# Start caputlogging
-epicsEnvSet("EPICS_AS_PUT_LOG_PV", "$(IOC):caPutLatest")
-caPutLogInit("localhost:7011")
 
 dbl > "$(TOP)/$(IOC)_PVs.list"
 
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-source /epics/base-3.15.5/require/3.0.0/bin/setE3Env.bash
+source /epics/base-7.0.1.1/require/3.0.4/bin/setE3Env.bash
 iocsh.bash labs-utgard-rack1-pdu1.cmd
