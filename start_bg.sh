959a5578502c90be6b181a730bcc2f805f2a58d9
Wayne Lewis <wayne.lewis@esss.se>
2019-02-11 14:48:11 +0100

    ENH: Change base version and telnet port

--- start_bg.sh
+++ start_bg.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 source /epics/base-7.0.1.1/require/3.0.4/bin/setE3Env.bash
 #nohup iocsh.bash labs-utgard-rack1-pdu1.cmd < /dev/zero &
-/usr/bin/procServ -f -L /var/log/procServ/labs-utgard-rack1-pdu1 -i ^C^D -c /var/run/procServ/labs-utgard-rack1-pdu1 2000 /epics/base-3.15.5/require/3.0.0/bin/iocsh.bash /epics/iocs/cmds/labs-utgard-rack1-pdu1/labs-utgard-rack1-pdu1.cmd &
+/usr/bin/procServ -f -L /var/log/procServ/labs-utgard-rack1-pdu1 -i ^C^D -c /var/run/procServ/labs-utgard-rack1-pdu1 2009 /epics/base-7.0.1.1/require/3.0.4/bin/iocsh.bash /epics/iocs/cmds/labs-utgard-rack1-pdu1/labs-utgard-rack1-pdu1.cmd &
